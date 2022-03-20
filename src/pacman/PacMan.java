8d456eea22416c1ec0758eec3c5390c1b6a3fd28
AaronGonzalez06 <aaron060812060812@gmail.com>
2022-03-20 16:54:50 +0100

    ventana

--- src/pacman/PacMan.java
+++ src/pacman/PacMan.java
@@ -4,17 +4,45 @@
  */
 package pacman;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /**
  *
  * @author Aaron
  */
-public class PacMan {
+public class PacMan extends JFrame {
+
+    public PacMan(){
+        ventana();
+
+    }
+    
+    
+    public void ventana(){
+        
+        this.setSize(1000, 1000);
+        //pone titulo a la ventana
+        setTitle("Pacman");
+        //nos hace el cierre de la ventana 
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setMinimumSize(new Dimension(700, 700));
+        this.setResizable(false);
+        this.getContentPane().setBackground(Color.BLACK);
+        this.setVisible(true);   
+    
+    
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        PacMan juego = new PacMan();
+        juego.setVisible(true);
     }
-    
+
 }
