fb79b7cce3adc897b576b38f7287408d630ac58f
AaronGonzalez06 <aaron060812060812@gmail.com>
2022-03-20 17:22:12 +0100

    ventana con titulo

--- src/pacman/PacMan.java
+++ src/pacman/PacMan.java
@@ -6,6 +6,10 @@ package pacman;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
 import javax.swing.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
@@ -15,25 +19,46 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
  */
 public class PacMan extends JFrame {
 
-    public PacMan(){
+    public PacMan() {
         ventana();
+        titulo();
 
     }
-    
-    
-    public void ventana(){
-        
+
+    public void ventana() {
+
         this.setSize(1000, 1000);
         //pone titulo a la ventana
         setTitle("Pacman");
         //nos hace el cierre de la ventana 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setMinimumSize(new Dimension(700, 700));
+        this.setMinimumSize(new Dimension(1000, 1000));
         this.setResizable(false);
         this.getContentPane().setBackground(Color.BLACK);
-        this.setVisible(true);   
-    
-    
+        this.setLayout(null);
+
+    }
+
+    public void titulo() {
+        JLabel titulo = new JLabel();
+        titulo.setText("PACMAN");
+        titulo.setBounds(300, 50, 400, 75);
+        titulo.setForeground(Color.YELLOW);
+        File fuente = new File("fuente/fuente.ttf");
+        try {
+            //crea la fuente
+            Font font = Font.createFont(Font.TRUETYPE_FONT, fuente);
+            //dar tanaño fuente
+            Font sizedFont = font.deriveFont(75f);
+            titulo.setFont(sizedFont);
+            titulo.setFont(sizedFont);
+        } catch (FontFormatException ex) {
+            System.err.println("error en font format");
+        } catch (IOException ex) {
+            System.err.println("error de entrada/salida");
+        }
+
+        this.add(titulo);
     }
 
     /**
